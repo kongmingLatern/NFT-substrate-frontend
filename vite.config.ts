7bae563820db92124bb059979316a24c4191a23a
凤之兮原 <1577310596@qq.com>
2023-01-17 20:58:14 +0800

    chore: add define to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env': {
+      ...process.env,
+      ...loadEnv('development', process.cwd()),
+    },
+  },
 })
