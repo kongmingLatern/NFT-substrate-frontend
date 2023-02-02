dda78558d886f9e42a944f57b31d898736b11c67
凤之兮原 <1577310596@qq.com>
2023-02-02 21:16:41 +0800

    chore: add unocss

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import 'uno.css'
 
 ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import Unocss from 'unocss/vite'
+import { presetAttributify, presetUno } from 'unocss';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), Unocss({
+    presets: [
+      presetUno(),
+      presetAttributify(),
+    ],
+  })],
   server: {
     hmr: true,
     watch: { usePolling: true },
