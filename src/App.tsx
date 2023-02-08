4b3bdbc415bfe787ba62c79c752f5df11d3c1994
凤之兮原 <1577310596@qq.com>
2023-02-08 20:20:24 +0800

    change: change foloder

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import AccountSelector from './AccountSelector'
 import { SubstrateContextProvider } from './substrate-lib'
 import { useEffect, useRef } from 'react'
-import WebcamFaceDetection from './face/WebcamFaceDetection'
+import WebcamFaceDetection from './component/WebcamFaceDetection'
 export default function App() {
   return (
     <>
