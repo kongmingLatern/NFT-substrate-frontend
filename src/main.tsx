689e2767edc04f47382fd82e0f09949ef19cf7e9
凤之兮原 <1577310596@qq.com>
2023-02-24 16:35:50 +0800

    update: config

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import ReactDOM from 'react-dom/client'
-import '@/assets/index.css'
-import '@/component/paint/i18n'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router'
+import '@/assets/index.css'
+import '@/assets/global.css'
+import '@/component/paint/i18n'
 
 ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
