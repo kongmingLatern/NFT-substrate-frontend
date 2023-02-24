d348f2b6c77a72e28bfb34c464567e976d4d61b5
凤之兮原 <1577310596@qq.com>
2023-02-24 15:34:41 +0800

    change: change home content

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,6 @@
 import Carousel from '@/component/common/Carousel'
 import Divider from '@/component/common/Divider'
 import Header from '@/component/common/Header'
-import Detail from '@/pages/Detail'
 import Create from './Create'
 
 export default function Home() {
