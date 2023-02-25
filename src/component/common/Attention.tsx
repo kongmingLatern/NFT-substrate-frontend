6f1d2ea1e40e7ed3348f9e39dfee6ca3f03dcc6f
ocean0hai <3067451183@qq.com>
2023-02-25 19:36:43 +0800

    feat:add shoppingcart

---
+++ src/component/common/Attention.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import {
+    Popover,
+    PopoverTrigger,
+    PopoverContent,
+    PopoverBody,
+    PopoverArrow,
+  } from '@chakra-ui/react'
+import { Icon } from '@iconify-icon/react'
+export default function Attention() {
+  return (
+    <>
+      <Popover trigger="hover">
+        <PopoverTrigger>
+            <button><Icon icon="ic:baseline-error-outline" /></button>
+        </PopoverTrigger>
+        <PopoverContent>
+            <PopoverArrow />
+            {/* <PopoverHeader>Confirmation!</PopoverHeader> */}
+            <PopoverBody>Are you sure you want to have that milkshake?</PopoverBody>
+        </PopoverContent>
+      </Popover>
+    </>
+  )
+}
--- src/component/personal/Headpicture.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Headpicture() {
-  return (
-    <div className='w-8/10'>
-      
-    </div>
-  )
-}
