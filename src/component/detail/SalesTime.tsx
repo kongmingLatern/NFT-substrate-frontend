c7d4ae9f0115ef4882b4e63b9e6d5910d9a68b1f
凤之兮原 <1577310596@qq.com>
2023-02-26 22:04:37 +0800

    refactor: salesTime.tsx

--- src/component/detail/SalesTime.tsx
+++ src/component/detail/SalesTime.tsx
@@ -1,21 +1,6 @@
+import { getCurrentDate } from '@/utils'
+
 export default function SalesTime() {
-  const time = new Date()
-  const year = time.getFullYear() //  返回的是年份
-  const month = time.getMonth() + 1 //  返回的月份上个月的月份，记得+1才是当月
-  const dates = time.getDate() //  返回的是几号
-  const hour = time.getHours()
-  const minutes = time.getMinutes()
-  const todaytime =
-    year +
-    '年' +
-    month +
-    '月' +
-    dates +
-    '日' +
-    ' ' +
-    hour +
-    ':' +
-    minutes
   const remaining = [
     { time: '01', id: 'hours' },
     { time: '02', id: 'minutes' },
@@ -23,7 +8,7 @@ export default function SalesTime() {
   ]
   return (
     <div className="font-thin">
-      <div>{todaytime}</div>
+      <div>{getCurrentDate()}</div>
       <div>
         {remaining.map(item => {
           return (
