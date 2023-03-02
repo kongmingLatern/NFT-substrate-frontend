758bf64a416c0d3edbdfd471d69201ce74e3f9b8
凤之兮原 <1577310596@qq.com>
2023-03-02 21:03:09 +0800

    feat: add spin unit-test

--- src/__tests__/spin.spec.tsx
+++ src/__tests__/spin.spec.tsx
@@ -11,7 +11,7 @@ describe('spin basic function', () => {
           class="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] flex justify-center items-center"
         >
           <div
-            class="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"
+            class="animate-spin rounded-full border-t-2 border-b-2 border-gray-900 h-16 w-16 "
           />
         </div>
         123
@@ -51,5 +51,59 @@ describe('spin basic function', () => {
         </div>
       </DocumentFragment>
     `)
+  })
+})
+
+describe('spin size function', () => {
+  it('size can be customed', () => {
+    const { asFragment } = render(
+      <Spin loading={true} size="middle"></Spin>
+    )
+    expect(asFragment()).toMatchInlineSnapshot(`
+      <DocumentFragment>
+        <div
+          class="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] flex justify-center items-center"
+        >
+          <div
+            class="animate-spin rounded-full border-t-2 border-b-2 border-gray-900 h-16 w-16 "
+          />
+        </div>
+      </DocumentFragment>
+    `)
+  })
+})
+
+describe('spin render function', () => {
+  it('render render function', () => {
+    const { asFragment } = render(
+      <Spin
+        loading={true}
+        render={() => <div>123</div>}
+      ></Spin>
+    )
+    expect(asFragment()).toMatchInlineSnapshot(
+      `
+      <DocumentFragment>
+        <div
+          class="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] flex justify-center items-center"
+        >
+          <div>
+            123
+          </div>
+        </div>
+      </DocumentFragment>
+    `
+    )
+  })
+  it('render render function', () => {
+    const { asFragment } = render(
+      <Spin
+        loading={false}
+        render={() => <div>123</div>}
+      ></Spin>
+    )
+    expect(asFragment()).toMatchInlineSnapshot(
+      '<DocumentFragment />'
+    )
   })
 })
